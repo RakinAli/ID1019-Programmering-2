7fe3fa851521e1a13094ec74c69aa7f3ab8781af
RakinAli <rakinali00@gmail.com>
2021-01-21 17:30:20 +0100

    fixed

--- Gitbook uppgifter/recursion.ex
+++ Gitbook uppgifter/recursion.ex
@@ -21,4 +21,34 @@ product of n and m:
       end
     end
   end
+
+  #Deals with Fibonnacci
+  def fib(x) do
+    case x do
+      0 -> 0
+      1 -> 1
+      _ -> fib(x-1) + fib(x-2)
+    end
+  end
+
+  #Exponents
+  def power(number,exponent) do
+    if exponent == 0 do
+      1
+    end
+
+    if exponent == 1 do
+      number
+    else
+      (prod(number,number) * power(number,exponent-1))/number
+    end
+  end
+
+  def ackerman(m,n) do
+    cond do
+      m==0 -> n+1
+      m>0 and n==0 -> ackerman(m-1,1)
+      m>0 and n>0 -> ackerman(m-1,ackerman(m,n-1))
+    end
+  end
 end
