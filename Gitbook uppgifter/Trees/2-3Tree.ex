f8d01780d8e9255b3f878cdbeeae8167dd668c84
RakinAli <rakinali00@gmail.com>
2021-01-30 18:10:10 +0100

    File organisation

---
+++ Gitbook uppgifter/Trees/2-3Tree.ex
@@ -0,0 +1,10 @@
+defmodule Tree do
+
+  @doc """
+  The empty tree -> :nil
+  a leaf -> {:leaf,key,value}
+  a two node:
+
+  """
+
+end
---
+++ Seminars/sem1/Huffman.ex
@@ -0,0 +1,26 @@
+defmodule Huffman do
+
+
+  def sample do
+    'the quick brown fox jumps over the lazy dog
+    this is a sample text that we will use when we build
+    up a table we will only handle lower case letters and
+    no punctuation symbols the frequency will of course not
+    represent english but it is probably not that far off'
+  end
+
+  def text() do
+    'this is something that we should encode'
+  end
+
+  def test() do
+    sample = sample()
+    tree = tree(sample)
+    encode = encode_table(tree)
+    decode = decode_table(tree)
+    text = text()
+    seq = encode(text, encode)
+    decode(seq, decode)
+  end
+
+end
