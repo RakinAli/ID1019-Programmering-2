1553cf7a33c2e5dd3df9c0e8ebf71aff331e22cf
RakinAli <rakinali00@gmail.com>
2021-02-06 13:49:04 +0100

    Create l6.ex

---
+++ Lectures/l6.ex
@@ -0,0 +1,16 @@
+defmodule Test do
+
+  #Lecture notes ->
+
+  # a domain:Z
+  # a set of primitive functions: +, -, *, mod, div
+  # Syntex: symbols, precedence, parantheese ie a way to write expressions
+
+  # (3+5) * (6-3) rewrite this
+  # A evulation of expressions
+
+  
+
+
+
+end
