37f0bbb0f24259213562a97ca69ffbad897c399f
RakinAli <rakinali00@gmail.com>
2021-02-21 14:16:07 +0100

    File movement

--- Lectures/l10-2.ex
+++ Lectures/l10-2.ex
@@ -1,4 +1,4 @@
-defmodule DAG do
+defmodule Graph do
   @doc """
   The dynamic programming approach
   _________________________________
@@ -24,22 +24,55 @@ defmodule DAG do
 
   ]
 
-  -> As a matrix of edges 
-
-
+  -> As a matrix of edges
   """
+
+  def sample() do
+    new([a: [b: 2,d: 5],b: [c: 2]])
+  end
+
+  def new(nodes) do
+    Map.new(nodes)
+  end
+
+  def next(from,map)do
+    Map.get(map,from,[])
+  end
+
 
   def shortest(from,from,_) do
     {0,[]}
   end
 
-  def shortest.from,to,graph) do
+  def shortest(from,to,graph) do
     next = Graph.next(from,graph)
     distances = distances(next,to,graph)
     select(distances)
   end
 
+  def distances(next,to,graph) do
+    Enum.map(next,fn({n,d})->
+      case shortest(n,to,graph) do
+        #Path not possible? Nil
+        {:inf,nil} -> {:inf,nil}
+        #Path possible? plus d with k
+        {k,path} -> {d+k,[n|path]}
+      end
+    end)
+  end
 
+  def select(distances) do
+    List.foldl(distances,
+    {:inf,nil},
+    fn({d,_}=s,{ad,_}=acc) ->
+      if d<ad do
+        s
+      else
+        acc
+
+      end
+    end)
+  end
 
 
 end
